b46cfa80881135f3c14baadd2b63294c6adaff68
Cindy Paredes <cindyrangel@Cindys-iMac.local>
2022-11-23 21:08:20 -0600

    updated the index from api and added the files for comments, and post

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,7 +1,10 @@
 const router = require('express').Router();
-
-const userRoutes = require ('.user-routes');
+const userRoutes = require ('./userRoutes');
+const commentsRoutes = require ('./commentsRoutes');
+const postRoutes = require ('./postRoutes');
 
 router.use('/user', userRoutes);
+router.use('/comments', commentsRoutes);
+router.use('/post', postRoutes);
 
 modeule.exports = router;
