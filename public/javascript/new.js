fb216bea9e2df5efbad77888db493c45a41a61e7
Cindy Paredes <cindyrangel@Cindys-iMac.local>
2022-12-04 21:30:05 -0600

    added new post js and handlebars

---
+++ public/javascript/new.js
@@ -0,0 +1,19 @@
+const newFromHandler = async function(event) {
+    event.preventDefault();
+
+    const postTitle = document.querySelector('input[name="post-title"]').value
+    const postContent = document.querySelector('textarea[name="post-body"]').value
+
+    await fetch("/api/post", {
+        method: "POST",
+        body: JSON.stringify({
+            postTitle,
+            postContent,
+        }),
+        headers: {"content-Type": "application/json"},
+    });
+}
+
+
+document.querySelector('#new-post-form')
+document.addEventListener('submit', newFormHandler);
