d36f94a50107bb77f197ffaa8c457c84396f8adf
Cindy Paredes <cindyrangel@Cindys-iMac.local>
2022-12-04 22:42:12 -0600

    fix typos

--- models/Post.js
+++ models/Post.js
@@ -2,7 +2,7 @@ const {
     Model,
     DataTypes
 } = require('sequelize');
-const sequelize = require('../config/connection');
+const sequelize = require('../config/config');
 
 
 class Post extends Model {}
